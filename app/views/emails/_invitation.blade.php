4e3b8fbcf0cb1e909eb4045778d95a377b4a8255
Cristian Conedera <cristian.conedera@bothmedia.com>
2014-08-28 17:38:17 -0300

    Frases Email Invitación

--- app/views/emails/_invitation.blade.php
+++ app/views/emails/_invitation.blade.php
@@ -19,7 +19,7 @@
 									<h3 class="center">Fuiste invitado al casamiento de</h3>
 									<img src="{{ $images[0] }}" class="ayc center" width="552" height="83" alt="Alejandra y Cristian" />
 									<p>Despues de tanto tiempo compartiendo nuestras vidas, decidimos dar el siguiente paso. Y nos encantaría compartir este gran momento con vos.</p>
-									<p>Por favor, hacé click en el siguiente botón para saber los detalles de la fiesta, como así también dirección y horarios. Es importante que ingreses para confirmar tu asistencia a la fiesta.</p>
+									<p>Por favor, hacé click en el siguiente botón para saber los detalles de la fiesta, como así también dirección y horarios. Es importante que ingreses para confirmar tu asistencia a la misma.</p>
 									<hr>
 									<p>
 										<table class="medium-button">
